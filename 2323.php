75f7bf7d5260c0e6700fab92ef069a6c2cc8850c
katyabosenok <katya.bosenok@mail.ru>
2017-12-06 19:51:30 +0700

    6/12

---
+++ 2323.php
@@ -0,0 +1,21 @@
+<?php
+$dbParams=require(
+	'db.php'
+);
+$db= new PDO(
+	"mysql:host=localhost;dbname=". 
+	$dbParams['database'].
+	";charset=utf8", 
+	$dbParams['username'],
+	$dbParams['password']
+	
+);
+//
+$students=$db
+	->query('SELECT * FROM `STUDENT`')
+	->fetchALL(PDO::FETCH_ASSOC);
+foreach ($students as $student){
+	echo "<p>".
+		htmlspecialchars($student['lastName']).
+		"</p>";
+}
